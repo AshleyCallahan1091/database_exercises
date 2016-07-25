008889159af3d35dc7c4b8876401ffc3596cced0
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-07-25 16:16:44 -0500

    classroom exercises

---
+++ albums_seeder.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+INSERT INTO albums (id, artist, name, release_date, sales, genre)
+VALUES ('1', 'Shania Twain', 'Come on Over', '1997', '29.6', 'Country, Pop');
+
+INSERT INTO albums (id, artist, name, release_date, sales, genre)
+VALUES ('2','Led Zeppelin', 'Led Zeppelin IV', '1971', '29.0', 'Hard Rock, Heavy Metal'),
+       ('3','Michael Jackson', 'Bad', '1987', '20.3', 'Pop, Funk, Rock');
+
+
+INSERT INTO albums (id, artist, name, release_date, sales, genre)
+VALUES ('4','Alanis Morissette', 'Jagged Little Pill', '1995', '24.8', 'Alternative Rock'),
+       ('5','Celine Dion', 'Falling into You', '1996', '20.2', 'Pop, Soft Rock');
+      
